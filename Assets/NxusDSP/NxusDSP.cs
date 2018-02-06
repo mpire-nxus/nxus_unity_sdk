06e6ebe8b4003a75e8bd1371e45710bcb24d00b3
SDK Development <sdkdevelopment@mpiremedia.com.au>
2018-02-06 10:43:11 +0100

    added predefined custom events

--- Assets/NxusDSP/NxusDSP.cs
+++ Assets/NxusDSP/NxusDSP.cs
@@ -48,6 +48,136 @@ public class NxusDSP : MonoBehaviour {
 		}
 	}
 
+	public static void trackEventInstall(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventInstall - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventInstall - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventInstall attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventInstall (attributesString);
+		}
+	}
+
+	public static void trackEventOpen(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventOpen - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventOpen - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventOpen attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventOpen (attributesString);
+		}
+	}
+
+	public static void trackEventRegistration(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventRegistration - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventRegistration - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventRegistration attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventRegistration (attributesString);
+		}
+	}
+
+	public static void trackEventPurchase(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventPurchase - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventPurchase - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventPurchase attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventPurchase (attributesString);
+		}
+	}
+
+	public static void trackEventLevel(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventLevel - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventLevel - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventLevel attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventLevel (attributesString);
+		}
+	}
+
+	public static void trackEventTutorial(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventTutorial - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventTutorial - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventTutorial attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventTutorial (attributesString);
+		}
+	}
+
+	public static void trackEventAddToCart(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventAddToCart - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventAddToCart - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventAddToCart attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventAddToCart (attributesString);
+		}
+	}
+
+	public static void trackEventCheckout(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventCheckout - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventCheckout - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventCheckout attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventCheckout (attributesString);
+		}
+	}
+
+	public static void trackEventInvite(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventInvite - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventInvite - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventInvite attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventInvite (attributesString);
+		}
+	}
+
+	public static void trackEventAchievement(Dictionary<string, string> attributes) {
+		Debug.Log ("NxusDSP trackEventAchievement - check instance");
+		if (NxusDSP.instance != null) {
+			Debug.Log ("NxusDSP trackEventAchievement - instance existing, tracking event");
+			string attributesString = "";
+			foreach(KeyValuePair<string, string> kvp in attributes) {
+				attributesString += kvp.Key + "=" + kvp.Value + "\n";
+			}
+			Debug.Log ("NxusDSP trackEventAchievement attributes: " + attributesString);
+			NxusDSP.instance.nxusDspTrackEventAchievement (attributesString);
+		}
+	}
+
 	#if UNITY_EDITOR
 	#region Public methods
 	public void nxusDspInitializeLibrary () {
@@ -61,28 +191,49 @@ public class NxusDSP : MonoBehaviour {
 	public void nxusDspTrackEventWithParams (string eventName, string attributes) {
 		// Empty
 	}
-    #endregion
 
-	#elif UNITY_WEBGL
-    #region Public methods
-    public void nxusDspInitializeLibrary()
-    {
-        // Empty
-    }
+	public void nxusDspTrackEventInstall (string attributes) {
+		// Empty
+	}
 
-    public void nxusDspTrackEvent(string eventName)
-    {
-        // Empty
-    }
+	public void nxusDspTrackEventOpen (string attributes) {
+		// Empty
+	}
 
-    public void nxusDspTrackEventWithParams(string eventName, string attributes)
-    {
-        // Empty
-    }
+	public void nxusDspTrackEventRegistration (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventPurchase (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventLevel (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventTutorial (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventAddToCart (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventCheckout (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventInvite (string attributes) {
+		// Empty
+	}
+
+	public void nxusDspTrackEventAchievement (string attributes) {
+		// Empty
+	}
     #endregion
 
 	#elif UNITY_IOS
-
     #region External methods
 	[DllImport("__Internal")]
 	private static extern void nxus_dsp_set_sdk_platform (string platform);
@@ -98,6 +249,36 @@ public class NxusDSP : MonoBehaviour {
 
 	[DllImport("__Internal")]
 	private static extern void nxus_dsp_track_event_with_params (string eventName, string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_install (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_open (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_registration (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_purchase (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_level (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_tutorial (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_add_to_cart (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_checkout (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_invite (string attributes);
+
+	[DllImport("__Internal")]
+	private static extern void nxus_dsp_track_event_achievement (string attributes);
     #endregion
 
     #region Public methods
@@ -114,6 +295,46 @@ public class NxusDSP : MonoBehaviour {
 
 	public void nxusDspTrackEventWithParams (string eventName, string attributes) {
 		nxus_dsp_track_event_with_params(eventName, attributes);
+	}
+
+	public void nxusDspTrackEventInstall (string attributes) {
+		nxus_dsp_track_event_install(attributes);
+	}
+
+	public void nxusDspTrackEventOpen (string attributes) {
+		nxus_dsp_track_event_open(attributes);
+	}
+
+	public void nxusDspTrackEventRegistration (string attributes) {
+		nxus_dsp_track_event_registration(attributes);
+	}
+
+	public void nxusDspTrackEventPurchase (string attributes) {
+		nxus_dsp_track_event_purchase(attributes);
+	}
+
+	public void nxusDspTrackEventLevel (string attributes) {
+		nxus_dsp_track_event_level(attributes);
+	}
+
+	public void nxusDspTrackEventTutorial (string attributes) {
+		nxus_dsp_track_event_tutorial(attributes);
+	}
+
+	public void nxusDspTrackEventAddToCart (string attributes) {
+		nxus_dsp_track_event_add_to_cart(attributes);
+	}
+
+	public void nxusDspTrackEventCheckout (string attributes) {
+		nxus_dsp_track_event_checkout(attributes);
+	}
+
+	public void nxusDspTrackEventInvite (string attributes) {
+		nxus_dsp_track_event_invite(attributes);
+	}
+
+	public void nxusDspTrackEventAchievement (string attributes) {
+		nxus_dsp_track_event_achievement(attributes);
 	}
     #endregion
 
@@ -146,6 +367,146 @@ public class NxusDSP : MonoBehaviour {
 
 		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
 		nxusDspTrackerClass.CallStatic("trackEvent", eventName, trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventInstall (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventInstall", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventOpen (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventOpen", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventRegistration (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventRegistration", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventPurchase (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventPurchase", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventLevel (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventLevel", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventTutorial (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventTutorial", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventAddToCart (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventAddToCart", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventCheckout (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventCheckout", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventInvite (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventInvite", trackingParamsClass);
+	}
+
+	public void nxusDspTrackEventAchievement (string attributes) {
+		AndroidJavaClass trackingParamsClass = new AndroidJavaClass("com.nxus.dsp.tracking.TrackingParams");
+
+		string[] attrs = attributes.Split (new string[] {"\n"}, System.StringSplitOptions.None);
+		foreach (string att in attrs) {
+			string[] attDetails = att.Split (new string[] {"="}, System.StringSplitOptions.None);
+			trackingParamsClass.Call ("put", attDetails[0], attDetails[1]);
+			Debug.Log ("NxusDSP Android Tracking Param: key=" + attDetails[0] + "; value=" + attDetails[1]);
+		}
+
+		AndroidJavaClass nxusDspTrackerClass = new AndroidJavaClass("com.nxus.dsp.NxusDSPTracker");
+		nxusDspTrackerClass.CallStatic("trackEventAchievement", trackingParamsClass);
 	}
     #endregion
 	#endif
--- Assets/NxusDSP/iOS/NxusDSPWrapper.m
+++ Assets/NxusDSP/iOS/NxusDSPWrapper.m
@@ -44,4 +44,174 @@ void nxus_dsp_track_event_with_params(char *eventName, const char *attributes) {
     [NxusDSP trackEvent:[NSString stringWithUTF8String:eventName] params:oAttributes];
 }
 
+void nxus_dsp_track_event_install(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventInstall:oAttributes];
+}
+
+void nxus_dsp_track_event_open(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventOpen:oAttributes];
+}
+
+void nxus_dsp_track_event_registration(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventRegistration:oAttributes];
+}
+
+void nxus_dsp_track_event_purchase(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventPurchase:oAttributes];
+}
+
+void nxus_dsp_track_event_level(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventLevel:oAttributes];
+}
+
+void nxus_dsp_track_event_tutorial(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventTutorial:oAttributes];
+}
+
+void nxus_dsp_track_event_add_to_cart(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventAddToCart:oAttributes];
+}
+
+void nxus_dsp_track_event_checkout(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventCheckout:oAttributes];
+}
+
+void nxus_dsp_track_event_invite(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventInvite:oAttributes];
+}
+
+void nxus_dsp_track_event_achievement(const char *attributes) {
+    NSString *attris = [NSString stringWithUTF8String:attributes];
+    NSArray *attributesArray = [attris componentsSeparatedByString:@"\n"];
+    
+    NSMutableDictionary *oAttributes = [[NSMutableDictionary alloc] init];
+    for (int i = 0; i < [attributesArray count]; i++) {
+        NSString *keyValuePair = [attributesArray objectAtIndex:i];
+        NSRange range = [keyValuePair rangeOfString:@"="];
+        if (range.location != NSNotFound) {
+            NSString *key = [keyValuePair substringToIndex:range.location];
+            NSString *value = [keyValuePair substringFromIndex:range.location+1];
+            [oAttributes setObject:value forKey:key];
+        }
+    }
+    [NxusDSP trackEventAchievement:oAttributes];
+}
+
 @end
